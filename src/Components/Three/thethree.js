30155d375b7409f4f32ab7dc872476caf60a086b
Thiha Ko Ko <thihasithu0224@gmail.com>
2023-02-18 10:06:23 +0630

    [feat: zooming 3d]

--- src/Components/Three/thethree.js
+++ src/Components/Three/thethree.js
@@ -22,22 +22,32 @@ const ThreeD = ({ getJoints, getPosition }) => {
   const getZRotation = (p1, p2) => {
     let e1 = Math.abs(p1.y);
     let e2 = Math.abs(p2.y);
-    return normalize(-80, 80, e2 - e1) - Math.PI / 2;
+    return normalize(-150, 150, e2 - e1) - Math.PI / 2;
   };
 
-  useFrame(() => {
+  useFrame((state) => {
     kp = getJoints();
+    // console.log(state);
     if (kp !== undefined && kp !== null) {
       kp.forEach((p) => {
+        let maskbox = p.box;
         let maskpoint = p.keypoints[6];
         let x = maskpoint.x;
         let y = maskpoint.y;
-        let z = maskpoint.z;
-        console.log(boxRef);
-        boxRef.current.position.x = ((x - 320) / 320) * 2;
-        boxRef.current.position.y = -((y - 240) / 240) * 2;
-        boxRef.current.position.z = (z / 100) * 2;
-        console.log(window.innerHeight);
+
+        let mskwidth = maskbox.width;
+
+        // console.log("this is z" + z);
+        console.log(boxRef.current.scale);
+        // console.log(mskwidth);
+
+        boxRef.current.scale.x = (mskwidth / 150) * 0.0008;
+        boxRef.current.scale.y = (mskwidth / 150) * 0.0008;
+        boxRef.current.scale.z = (mskwidth / 150) * 0.0008;
+
+        boxRef.current.position.x = ((x - 320) / 320) * 1.6;
+        boxRef.current.position.y = -((y - 240) / 240) * 1.5;
+
         boxRef.current.rotation.y = -getYRotation(
           p.keypoints[143],
           p.keypoints[372],
@@ -52,8 +62,8 @@ const ThreeD = ({ getJoints, getPosition }) => {
   });
   return (
     <>
-      <group ref={boxRef}>
-        <mesh scale={[0.0008, 0.0008, 0.0008]}>
+      <group>
+        <mesh ref={boxRef} scale={[0.0008, 0.0008, 0.0008]}>
           <primitive object={fbx} />
           <meshStandardMaterial color={"blue"} />
         </mesh>
